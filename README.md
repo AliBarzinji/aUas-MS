dd6561cd9187e442273ff733dd49e8666141a929
Ali Barzinji <97831010+AliBarzinji@users.noreply.github.com>
2026-05-10 17:56:35 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# University Management System
+
+version: 1.0.0
+
+## TECHNOLOGIES
+
+1. PHP
+1. MYSQL
+1. BOOTSTRAP 5
+1. HTML
+1. CSS
+1. JS
---
+++ sms_db.sql
@@ -0,0 +1,447 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 09, 2026 at 11:01 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sms_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `username` varchar(127) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fname` varchar(127) NOT NULL,
+  `lname` varchar(127) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `username`, `password`, `fname`, `lname`) VALUES
+(1, 'ali', '$2a$10$7vo0cVug33OPRX7Z6eUXv.l/BTi8jxRwXsfDQ2JTVtKu9TzVY2X8K', 'Ali', 'Barzinji');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class`
+--
+
+CREATE TABLE `class` (
+  `class_id` int(11) NOT NULL,
+  `grade` int(11) NOT NULL,
+  `section` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `class`
+--
+
+INSERT INTO `class` (`class_id`, `grade`, `section`) VALUES
+(1, 7, 2),
+(2, 1, 1),
+(3, 3, 3),
+(4, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grades`
+--
+
+CREATE TABLE `grades` (
+  `grade_id` int(11) NOT NULL,
+  `grade` varchar(31) NOT NULL,
+  `grade_code` varchar(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `grades`
+--
+
+INSERT INTO `grades` (`grade_id`, `grade`, `grade_code`) VALUES
+(1, '1', 'G'),
+(2, '2', 'G'),
+(3, '1', 'KG'),
+(4, '2', 'KG'),
+(7, '3', 'G');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `message_id` int(11) NOT NULL,
+  `sender_full_name` varchar(100) NOT NULL,
+  `sender_email` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `date_time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `message`
+--
+
+INSERT INTO `message` (`message_id`, `sender_full_name`, `sender_email`, `message`, `date_time`) VALUES
+(4, 'Test', 'aliameenhaider554@gmail.com', 'hello this is for testing', '2026-05-09 09:48:57');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registrar_office`
+--
+
+CREATE TABLE `registrar_office` (
+  `r_user_id` int(11) NOT NULL,
+  `username` varchar(127) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fname` varchar(31) NOT NULL,
+  `lname` varchar(31) NOT NULL,
+  `address` varchar(31) NOT NULL,
+  `employee_number` int(11) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `phone_number` varchar(31) NOT NULL,
+  `qualification` varchar(31) NOT NULL,
+  `gender` varchar(7) NOT NULL,
+  `email_address` varchar(255) NOT NULL,
+  `date_of_joined` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `registrar_office`
+--
+
+INSERT INTO `registrar_office` (`r_user_id`, `username`, `password`, `fname`, `lname`, `address`, `employee_number`, `date_of_birth`, `phone_number`, `qualification`, `gender`, `email_address`, `date_of_joined`) VALUES
+(1, 'adel', '$2a$10$9bwSdFXr.PXYFpMXIF8jIu8jZurmVIFMhDEI/lYPIzi0JksAd0cDi', 'Adel', 'Ali', 'Duhok', 843583, '2012-10-02', '+9647501234567', 'doctor', 'Male', 'adel.zebari@abc.com', '2026-05-05 01:03:25'),
+(2, 'ibrahim', '$2a$10$e6WZ9xcQ2Zfsj2BdfWy5FOXj2VJhkpQR6YdgyCf3Z5tq/8Agous3C', 'Ibrahim', 'Mahmood', 'Akre', 6546, '1990-06-05', '+9647501234567', 'doctor', 'Male', 'ibrahim.zebari@abc.com', '2025-11-05 23:06:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `section`
+--
+
+CREATE TABLE `section` (
+  `section_id` int(11) NOT NULL,
+  `section` varchar(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `section`
+--
+
+INSERT INTO `section` (`section_id`, `section`) VALUES
+(1, 'A'),
+(2, 'B'),
+(3, 'C'),
+(6, 'D');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `setting`
+--
+
+CREATE TABLE `setting` (
+  `id` int(11) NOT NULL,
+  `current_year` int(11) NOT NULL,
+  `current_semester` varchar(11) NOT NULL,
+  `school_name` varchar(100) NOT NULL,
+  `slogan` varchar(300) NOT NULL,
+  `about` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `setting`
+--
+
+INSERT INTO `setting` (`id`, `current_year`, `current_semester`, `school_name`, `slogan`, `about`) VALUES
+(1, 2026, 'VI', 'Akre University', 'Excellence in Education, Empowering Minds, Shaping Futures.', 'At Akre University, we are dedicated to providing a transformative educational experience that empowers our students to excel in their chosen fields. ');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `student_id` int(11) NOT NULL,
+  `username` varchar(127) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fname` varchar(127) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `grade` int(11) NOT NULL,
+  `section` int(11) NOT NULL,
+  `address` varchar(31) NOT NULL,
+  `gender` varchar(7) NOT NULL,
+  `email_address` varchar(255) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `date_of_joined` timestamp NULL DEFAULT current_timestamp(),
+  `parent_fname` varchar(127) NOT NULL,
+  `parent_lname` varchar(127) NOT NULL,
+  `parent_phone_number` varchar(31) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`student_id`, `username`, `password`, `fname`, `lname`, `grade`, `section`, `address`, `gender`, `email_address`, `date_of_birth`, `date_of_joined`, `parent_fname`, `parent_lname`, `parent_phone_number`) VALUES
+(1, 'avraz', '$2a$10$/Swm4n6lpFgQBgCi.VsTX.cGnqvTQG8tmvIgAEdKVEcsSegKqJltS', 'Avraz', 'Azad', 1, 1, 'Grasin', 'Male', 'avraz.harki@abc.com', '2012-09-12', '2026-04-15 14:16:44', 'Azad', 'Omar', '9647501234567'),
+(3, 'hiwa', '$2a$10$DCBCErqhcR9bCQWo4k8oGOMVuopHZjr0oom3Bmkf78paiP4VgXGOG', 'Hiwa', 'Taha', 2, 1, 'Akri-Aylull', 'Male', 'hiwa.akrayi@abc.com', '2002-12-03', '2026-03-11 14:16:51', 'Taha', 'Qasim', '9647501234567'),
+(4, 'varin', '$2a$10$k2IhCzWbgUIZjaCX/BnID.cTas48GfnDBbV8q3k5Wj3QQOEBEjkNW', 'Varin', 'Haider', 1, 1, 'Bardarash', 'Female', 'vana.barzinji@abc.com', '2023-06-14', '2026-02-02 13:48:49', 'Haider', 'Amin', '9647501234567'),
+(5, 'abrar', '$2a$10$sb3CTlRCk9zGhM7qqhe4DODzLau6ge4fzr7Co0jfRkziu.4rMKkIC', 'Abrar', 'Abdullah', 1, 1, 'Bardarash', 'Male', 'abrar.botani@abc.com', '2000-02-02', '2023-02-12 18:11:26', 'Abdullah', 'Ahmed', '9647501234567');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_score`
+--
+
+CREATE TABLE `student_score` (
+  `id` int(11) NOT NULL,
+  `semester` varchar(100) NOT NULL,
+  `year` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `results` varchar(512) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student_score`
+--
+
+INSERT INTO `student_score` (`id`, `semester`, `year`, `student_id`, `teacher_id`, `subject_id`, `results`) VALUES
+(1, 'II', 2021, 1, 1, 1, '10 15,15 20,10 10,10 20,30 35'),
+(2, 'II', 2023, 1, 1, 4, '15 20,4 5'),
+(3, 'I', 2022, 1, 1, 5, '10 20,50 50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `subject_id` int(11) NOT NULL,
+  `subject` varchar(31) NOT NULL,
+  `subject_code` varchar(31) NOT NULL,
+  `grade` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`subject_id`, `subject`, `subject_code`, `grade`) VALUES
+(1, 'English', 'En', 1),
+(2, 'Physics', 'Phy', 2),
+(3, 'Biology', 'Bio-01', 1),
+(4, 'Math', 'Math-01', 1),
+(5, 'Chemistry', 'ch-01', 1),
+(6, 'Programming', 'pro-01', 1),
+(7, 'Java', 'java-01', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachers`
+--
+
+CREATE TABLE `teachers` (
+  `teacher_id` int(11) NOT NULL,
+  `username` varchar(127) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `class` varchar(31) NOT NULL,
+  `fname` varchar(127) NOT NULL,
+  `lname` varchar(127) NOT NULL,
+  `subjects` varchar(31) NOT NULL,
+  `address` varchar(31) NOT NULL,
+  `employee_number` int(11) NOT NULL,
+  `date_of_birth` date DEFAULT NULL,
+  `phone_number` varchar(31) NOT NULL,
+  `qualification` varchar(127) NOT NULL,
+  `gender` varchar(7) NOT NULL,
+  `email_address` varchar(255) NOT NULL,
+  `date_of_joined` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `teachers`
+--
+
+INSERT INTO `teachers` (`teacher_id`, `username`, `password`, `class`, `fname`, `lname`, `subjects`, `address`, `employee_number`, `date_of_birth`, `phone_number`, `qualification`, `gender`, `email_address`, `date_of_joined`) VALUES
+(1, 'hajar', '$2a$10$KvvRd0sG.1BCISXc8AgCB.ejfbhFuGUXtcQKvgCIT2YxTtpmbi8FO', '1234', 'Hajar', 'Massih', '1245', 'Duhok', 6546, '2026-05-05', '9647501234567', 'Dr', 'Male', 'hajar.massih.2026@abc.com', '2022-09-09 05:23:45'),
+(5, 'adel', '$2a$10$twVWX.OgWvX8IEv8V21sn.XKnGZA3U4j5zrvEQ5zIRVauxAL/eQ9q', '123', 'Adel', 'Ali', '12', 'Duhok', 1929, '2005-09-09', '9647500123456', 'Dr', 'Male', 'adel.zebari.2026@abc.com', '2022-09-09 06:42:31');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `class`
+--
+ALTER TABLE `class`
+  ADD PRIMARY KEY (`class_id`);
+
+--
+-- Indexes for table `grades`
+--
+ALTER TABLE `grades`
+  ADD PRIMARY KEY (`grade_id`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`message_id`);
+
+--
+-- Indexes for table `registrar_office`
+--
+ALTER TABLE `registrar_office`
+  ADD PRIMARY KEY (`r_user_id`);
+
+--
+-- Indexes for table `section`
+--
+ALTER TABLE `section`
+  ADD PRIMARY KEY (`section_id`);
+
+--
+-- Indexes for table `setting`
+--
+ALTER TABLE `setting`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `student_score`
+--
+ALTER TABLE `student_score`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`subject_id`);
+
+--
+-- Indexes for table `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`teacher_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `class`
+--
+ALTER TABLE `class`
+  MODIFY `class_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `grades`
+--
+ALTER TABLE `grades`
+  MODIFY `grade_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `registrar_office`
+--
+ALTER TABLE `registrar_office`
+  MODIFY `r_user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `section`
+--
+ALTER TABLE `section`
+  MODIFY `section_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `setting`
+--
+ALTER TABLE `setting`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `student_score`
+--
+ALTER TABLE `student_score`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `teachers`
+--
+ALTER TABLE `teachers`
+  MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
